c33c7f853bd45f80d46435a2e24436b026088f90
PierreGauthier <p_gauthier@live.fr>
2014-02-04 19:12:31 +0100

    Finition header

--- gestionAbsence/admin.py
+++ gestionAbsence/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from gestionAbsence.models import Enseignant
+from gestionAbsence.models import *
 
+admin.site.register(Annee)
+admin.site.register(Departement)
 admin.site.register(Enseignant)
+admin.site.register(Secretaire)
+admin.site.register(Etudiant)
--- gestionAbsence/models.py
+++ gestionAbsence/models.py
@@ -4,23 +4,19 @@ from django.contrib.auth.models import User
 
 class Enseignant(models.Model):
     user = models.OneToOneField(User)  
-    nom = models.CharField(max_length=30)
-    prenom = models.CharField(max_length=30) 
 
 class Secretaire(models.Model):
     user = models.OneToOneField(User)  
-    nom = models.CharField(max_length=30)
-    prenom = models.CharField(max_length=30)
 
 class Departement(models.Model):
+    nom = models.CharField(max_length=20, primary_key=True)
     respo = models.OneToOneField(Enseignant)
 
 class Annee(models.Model):
+    nom = models.CharField(max_length=20, primary_key=True)
     respo = models.OneToOneField(Enseignant)
     dept = models.ForeignKey(Departement)
 
 class Etudiant(models.Model):
     user = models.OneToOneField(User)  
-    annee = models.ForeignKey(Annee)
-    nom = models.CharField(max_length=30)
-    prenom = models.CharField(max_length=30) 
+    annee = models.ForeignKey(Annee) 
