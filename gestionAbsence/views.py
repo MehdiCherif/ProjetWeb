881fb0eddcbc12eee90574fe8d55628b910e8501
PierreGauthier <p_gauthier@live.fr>
2014-02-11 19:22:45 +0100

    SearchEtu

--- gestionAbsence/views.py
+++ gestionAbsence/views.py
@@ -9,6 +9,16 @@ def accueil(request):
     return render(request, 'base.html',{"titre":"COOOO Accueil de PolyAbs"});
   else :
     return render(request, 'base.html',{"titre":"Accueil de PolyAbs"});
+
+def searchEtu(request, nom, prenom):
+  etuList = Etudiants.objects.all().filter(user__last_name = nom, user__first_name = prenom)
+  
+  page = '<table>'
+  
+  for etu in etuList :
+    page = page + '<tr><td><a href="/info/etu/' + etu.user.id + '">' + etu.user.last_name + " " + etu.user.first_name + "</a></td></tr>"
+    return HttpResponseRedirect(page)
+  
   
 def log_in(request):
 	logout(request)
