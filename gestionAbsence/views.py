16ae9eed1f922037bbb8459b8569707ca496a417
PierreGauthier <p_gauthier@live.fr>
2014-02-15 14:07:38 +0100

    Debut ensiegnant
    If sur les groupes dans le view
    Liste des cours par enseignant

--- gestionAbsence/views.py
+++ gestionAbsence/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
+from gestionAbsence.models import *
 
 # Create your views here.
 
 def accueil(request):
   if request.user.is_authenticated():
-    return render(request, 'base.html',{"titre":"COOOO Accueil de PolyAbs"});
+    groupe = request.user.groups.all()
+    if groupe[0]:
+      groupe = groupe[0].name
+      
+    if groupe == 'Enseignant':
+      list_absence = Cours.objects.all()
+      return render(request, 'enseignant.html',{"titre":"Accueil de PolyAbs", 'list_absence':list_absence})
+    elif groupe == 'Etudiant':
+      return render(request, 'etudiant.html',{"titre":"Accueil de PolyAbs"})
+    elif groupe == 'Secretaire':
+      return render(request, 'secretaire.html',{"titre":"Accueil de PolyAbs"})
+      
   else :
-    return render(request, 'base.html',{"titre":"Accueil de PolyAbs"});
+    return render(request, 'base.html',{"titre":"Accueil de PolyAbs"})
+
+def enseignant_cours(request, cours_id):
+  cours = Cours.objects.get(cours_id)
+  return render(request, 'enseignant_cours.html',{"titre":"Accueil de PolyAbs", 'cours':cours})
 
 def searchEtu(request, nom, prenom):
   etuList = Etudiants.objects.all().filter(user__last_name = nom, user__first_name = prenom)
